b45df3f8010befe93c5f857f781f51f81d010299
subash rai <subashbelina255@gmail.com>
2025-05-01 11:44:52 +0200

    Fix MIME type issues and build configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@ export default defineConfig({
   base: '/e-plantShopping/',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`
   }
 })
