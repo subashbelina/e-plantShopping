74c3022556500d9a8f63256000ad74873188fd60
subash rai <subashbelina255@gmail.com>
2025-05-01 11:06:18 +0200

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: '/e-plantShopping/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
